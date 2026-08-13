fde34dfe8c2443adedc0db390653e16f7cd71fa6
Sanjay G V <sanjaygv47@gmail.com>
2026-08-13 21:09:10 +0530

    Time: 665 ms (11.91%), Space: 0B (100%) - LeetHub

--- 0584-find-customer-referee/0584-find-customer-referee.sql
+++ 0584-find-customer-referee/0584-find-customer-referee.sql
@@ -1,3 +1,5 @@
-# Write your MySQL query statement below
-select name from Customer
-where (referee_id !=2 OR referee_id IS NULL)
+Select c1.name from
+Customer c1
+Left Join Customer c2
+On c1.id = c2.id
+Where c1.referee_id != 2 OR c1.referee_id IS NULL
