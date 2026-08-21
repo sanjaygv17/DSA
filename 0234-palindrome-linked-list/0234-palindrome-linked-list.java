716e3a8bae33945abc894f30014b0cc17cfc7413
Sanjay G V <sanjaygv47@gmail.com>
2026-08-21 20:06:44 +0530

    Time: 4 ms (66.91%), Space: 94 MB (96.13%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -9,42 +9,39 @@
  * }
  */
 class Solution {
-    
     public boolean isPalindrome(ListNode head) {
-        if (head == null || head.next == null) {
-            return true;
-        }
-        ListNode slow = head;
-        ListNode fast = head;
+        ListNode slow=head;
+        ListNode fast=head;
 
-        while (fast != null && fast.next != null) {
-            slow = slow.next;
-            fast = fast.next.next;
+        while( fast != null && fast.next != null){
+            slow=slow.next;
+            fast=fast.next.next;
         }
-        ListNode secondHalf = reverseList(slow);
-        ListNode first = head;
-        ListNode second = secondHalf;
+        ListNode secondHalf=reverse(slow);
 
-        while (second != null) {
-            if (first.val != second.val) {
+        ListNode first=head;
+
+        while(secondHalf != null){
+            if(first.val != secondHalf.val){
                 return false;
             }
-            first = first.next;
-            second = second.next;
+            first=first.next;
+            secondHalf=secondHalf.next;
         }
-
-        return true;   
+        return true;
+        
     }
-    private ListNode reverseList(ListNode head) {
-        ListNode prev = null;
-        ListNode curr = head;
-
-        while (curr != null) {
-            ListNode next = curr.next;
-            curr.next = prev;
-            prev = curr;
-            curr = next;
+    ListNode reverse(ListNode node){
+        ListNode prev=null;
+        ListNode current=node;
+        while(current != null){
+            ListNode next=current.next;
+            current.next=prev;
+            prev=current;
+            current=next;
         }
         return prev;
-    } 
+    
+    }
+   
 }
